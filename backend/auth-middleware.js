d67efd63051917d3398c73300c1239d4d33b4328
darktmeplarr <fullstackdeveloper@onlinegamingsoft>
2020-12-12 14:46:59 +0300

    json web token authentication

--- backend/auth-middleware.js
+++ backend/auth-middleware.js
@@ -1,50 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const config = require("./config.json");
 
-const app_secret = "myappsecret";
-
-module.exports = function(req, res, next) {
-
-    let token = req.headers['authorization'];
-    if (token != null && token.startsWith('Bearer<')) {
-        token = token.substring(7, token.lenght);
-        try {
-            jwt.verify(token, app_secret);
-            next();
-            return;
-        } catch (err) {
-            res.statusCode = 401;
-            return res.end();
-
-        }
+module.exports = function (req, res, next) {
+  let token = req.headers["authorization"];
+  console.log(req.headers["authorization"]);
+  if (token) {
+    console.log("tokennn");
+    try {
+      jwt.verify(token, config.app_secret);
+      next();
+      console.log("next");
+      return;
+    } catch (err) {
+      res.statusCode = 401;
+      return res.json({ error: err });
     }
-
-
-    // if (req.url === '/login' && req.method == 'POST') {
-    //     if (req.body.username === username && req.body.password === password) {
-    //         let token = jwt.sign({ data: username, expiresIn: '1h' }, app_secret);
-    //         res.json({ sucess: true, token: token });
-    //     } else {
-    //         res.json({ sucess: false });
-    //     }
-    //     res.end();
-    //     return;
-    // } else {
-    //     if ((req.url.startsWith("/places") || req.url.startsWith("/categories")) && (req.method != 'GET')) {
-    //         let token = req.headers['authorization'];
-    //         if (token != null && token.startsWith('Bearer<')) {
-    //             token = token.substring(7, token.lenght);
-    //             try {
-    //                 jwt.verify(token, app_secret);
-    //                 next();
-    //                 return;
-    //             } catch (err) {}
-    //         }
-    //         res.statusCode = 401;
-    //         res.end();
-    //         return;
-    //     }
-
-
-}
-next();
-}
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 
 const bodyParser = require("body-parser");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const Authentication = require("./auth-middleware");
+const config = require("./config.json");
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -14,227 +16,194 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost/tc", { useNewUrlParser: true });
 
+const multer = require("multer");
 
-const multer = require('multer')
-
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "c:/Users/ozgrg/Desktop/web/Mean/backend/public/uploads");
-    },
+  destination: function (req, file, cb) {
+    cb(null, "c:/Users/ozgrg/Desktop/web/Mean/backend/public/uploads");
+  },
 
-    filename: function(req, file, cb) {
-        cb(
-            null,
-            file.originalname
-        );
-    }
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 const upload = multer({ storage: storage });
 
 /*------------PLACES-------------*/
 
 const Place = mongoose.model(
-    "places",
-    new mongoose.Schema({
-
-        name: String,
-        sort_no: String,
-        category: { type: mongoose.Schema.Types.ObjectId, ref: 'categories' },
-        grup: String,
-        perma: String,
-        secim_baslik: String,
-        secim_spot: String,
-        description: String,
-        popular: String,
-        transfree: Boolean,
-        meta_title: String,
-        meta_description: String,
-        status: Boolean,
-        main_image: Array,
-        images: Array
-    })
+  "places",
+  new mongoose.Schema({
+    name: String,
+    sort_no: String,
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "categories" },
+    grup: String,
+    perma: String,
+    secim_baslik: String,
+    secim_spot: String,
+    description: String,
+    popular: String,
+    transfree: Boolean,
+    meta_title: String,
+    meta_description: String,
+    status: Boolean,
+    main_image: Array,
+    images: Array,
+  })
 );
 
-app.post("/places", async(req, res) => {
-    const { _id, name, sort_no, category, grup, perma, secim_baslik, secim_spot, description, popular, transfree, meta_title, meta_description, status, main_image, images } = req.body;
+app.post("/login", async (req, res) => {
+  const token = jwt.sign({ userId: "asldkhsakldjasl" }, config.app_secret);
 
+  res.json(token);
+});
 
+app.post("/places", async (req, res) => {
+  const {
+    _id,
+    name,
+    sort_no,
+    category,
+    grup,
+    perma,
+    secim_baslik,
+    secim_spot,
+    description,
+    popular,
+    transfree,
+    meta_title,
+    meta_description,
+    status,
+    main_image,
+    images,
+  } = req.body;
 
+  // if (!name || !surname) {
+  //     return res.json({ message: "Error" });
+  // }
 
-    // if (!name || !surname) {
-    //     return res.json({ message: "Error" });
-    // }
+  const place = new Place({
+    _id,
+    name,
+    sort_no,
+    category,
+    grup,
+    perma,
+    secim_baslik,
+    secim_spot,
+    description,
+    popular,
+    transfree,
+    meta_title,
+    meta_description,
+    status,
+    main_image,
+    images,
+  });
 
-    const place = new Place({
-        _id,
-        name,
-        sort_no,
-        category,
-        grup,
-        perma,
-        secim_baslik,
-        secim_spot,
-        description,
-        popular,
-        transfree,
-        meta_title,
-        meta_description,
-        status,
-        main_image,
-        images
+  await place.save();
+
+  return res.json({ place });
+});
+
+app.get("/places", async (req, res) => {
+  const places = await Place.find().populate("category").exec();
+
+  return res.json(places);
+});
+
+app.get("/places/:_id", async (req, res) => {
+  const place = await Place.findById(req.params._id).exec();
+
+  return res.json(place);
+});
+
+app.delete("/place/:placeId", async (req, res) => {
+  const deletedPlace = await Place.deleteOne({ _id: req.params.placeId });
+
+  return res.json(deletedPlace);
+});
+
+app.put("/place", async (req, res) => {
+  const { place } = req.body;
+
+  const updatedPlace = await Place.findByIdAndUpdate(
+    { _id: place._id },
+    place
+  ).exec();
+
+  return res.json({
+    place: updatedPlace,
+  });
+});
+
+app.post(
+  "/upload-image",
+  upload.array("images", 20),
+  function (req, res, next) {
+    return res.json({
+      images: [...req.files.map((file) => file.originalname)],
     });
+  }
+);
 
-    await place.save();
-
-    return res.json({ place });
+app.post("/upload-image", function (req, res, next) {
+  return res.json({
+    main_image: [...req.files.map((file) => file.originalname)],
+  });
 });
-
-app.get("/places", async(req, res) => {
-    const places = await Place.find().
-    populate("category")
-        .exec();
-
-    return res.json(
-        places
-    );
-});
-
-
-app.get("/places/:_id", async(req, res) => {
-    const place = await Place.findById(req.params._id).exec();
-
-    return res.json(
-        place
-    );
-});
-
-app.delete("/place/:placeId", async(req, res) => {
-
-    const deletedPlace = await Place.deleteOne({ _id: req.params.placeId })
-
-    return res.json(deletedPlace)
-
-})
-
-app.put("/place", async(req, res) => {
-    const { place } = req.body
-
-    const updatedPlace = await Place.findByIdAndUpdate({ _id: place._id }, place).exec()
-
-    return res.json({
-        place: updatedPlace
-    })
-
-})
-
-app.post('/upload-image', upload.array('images', 20), function(req, res, next) {
-
-    return res.json({
-        images: [
-            ...req.files.map(file => file.originalname)
-        ]
-    })
-})
-
-
-app.post('/upload-image', function(req, res, next) {
-
-    return res.json({
-        main_image: [
-            ...req.files.map(file => file.originalname)
-        ]
-    })
-})
-
-
-
 
 /*-----------CATEGORIES----------*/
 
 const Category = mongoose.model(
-    "categories",
-    new mongoose.Schema({
-        name: String
-    })
+  "categories",
+  new mongoose.Schema({
+    name: String,
+  })
 );
 
-app.post("/categories", async(req, res) => {
-    const { _id, name } = req.body;
+app.post("/categories", Authentication, async (req, res) => {
+  const { name } = req.body;
 
-    // if (!name || !surname) {
-    //     return res.json({ message: "Error" });
-    // }
+  const category = new Category({
+    name,
+  });
 
-    const category = new Category({
-        _id,
-        name
-    });
+  await category.save();
 
-    await category.save();
-
-    return res.json({ category });
+  return res.json({ category });
 });
 
-app.get("/categories", async(req, res) => {
-    const categories = await Category.find().exec();
+app.get("/categories", async (req, res) => {
+  const categories = await Category.find().exec();
 
-    return res.json(
-        categories
-    );
+  return res.json(categories);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get("/categories/:_id", async(req, res) => {
-    const category = await await Category.findById(req.params._id).exec();
-    return res.json(
-        category
-    );
+app.get("/categories/:_id", async (req, res) => {
+  const category = await await Category.findById(req.params._id).exec();
+  return res.json(category);
 });
 
-app.delete("/category/:categoryId", async(req, res) => {
-    const deleteCategory = await Category.deleteOne({ _id: req.params.categoryId })
-    return res.json(deletedCategory)
-})
+app.delete("/category/:categoryId", async (req, res) => {
+  const deleteCategory = await Category.deleteOne({
+    _id: req.params.categoryId,
+  });
+  return res.json(deletedCategory);
+});
 
-app.put("/category", async(req, res) => {
-    const { category } = req.body
-    console.log(category)
+app.put("/category", async (req, res) => {
+  const { category } = req.body;
 
-    const updatedCategory = await (await Category.findByIdAndUpdate({ _id: category._id }, category)).exec()
-    return res.json({
-        category: updatedCategory
-    })
-})
-
-
-
+  const updatedCategory = await (
+    await Category.findByIdAndUpdate({ _id: category._id }, category)
+  ).exec();
+  return res.json({
+    category: updatedCategory,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
